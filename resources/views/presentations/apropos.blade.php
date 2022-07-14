6c9c8ed1f514bd1d3f523387b27c08f92cbfc5c1
Mohamed Camara <mohamed669970215@gmail.com>
2022-07-14 18:58:35 +0000

    La section yield avec le titre pour toutes les pages

--- resources/views/presentations/apropos.blade.php
+++ resources/views/presentations/apropos.blade.php
@@ -1,4 +1,5 @@
 @extends('base')
+@section('title', 'Learning|Apropos')
 @section('content')
     <p>Cette appliation à été consue par Laite&hearts;Yagami</p>
     <p><a href="/">Retour sur la page d'acceuil</a></p>
