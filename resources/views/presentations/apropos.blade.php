fd6a9e324a2274730bf4661d7e39d36ec1c863de
Mohamed Camara <mohamed669970215@gmail.com>
2022-07-14 19:14:58 +0000

    Variable d'environnement avc APP_NAME pour le nom global de notre application

--- resources/views/presentations/apropos.blade.php
+++ resources/views/presentations/apropos.blade.php
@@ -1,5 +1,5 @@
 @extends('base')
-@section('title', 'Learning|Apropos')
+@section('title', 'Apropos|'. env('APP_NAME'))
 @section('content')
     <p>Cette appliation à été consue par Laite&hearts;Yagami</p>
     <p><a href="/">Retour sur la page d'acceuil</a></p>
