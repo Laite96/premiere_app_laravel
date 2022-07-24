0cf0975eeed064f8aa3838208f4db9c873221fe7
Mohamed Camara <mohamed669970215@gmail.com>
2022-07-24 14:58:00 +0000

    Ajout des images

--- resources/views/presentations/apropos.blade.php
+++ resources/views/presentations/apropos.blade.php
@@ -1,6 +1,7 @@
 @extends('base')
 @section('title', 'Apropos|'. config('app.name'))
 @section('content')
+    <img src="/img/ly.jpg" alt="">
     <p>Cette appliation à été consue par Laite&hearts;Yagami</p>
     <p><a href="{{route('home')}}">Retour sur la page d'acceuil</a></p>
 @endsection
--- resources/views/presentations/home.blade.php
+++ resources/views/presentations/home.blade.php
@@ -1,5 +1,6 @@
 @extends('base')
 @section('content')
+    <img src="/img/flag_guinee.png" alt="Drapeau de la Guinée">
         <h1>Salut! Je suis Laite Yagami je viens de la Guinée</h1>
         <p>Il est {{date('H:i A')}} à Conakry</p>
 @endsection
