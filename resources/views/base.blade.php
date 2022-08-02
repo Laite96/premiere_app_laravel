00a2e7e5db48ac46321de39ce476d5f25287e983
Mohamed Camara <mohamed669970215@gmail.com>
2022-08-02 00:59:53 +0000

    Creation d'une fonction helpers pour encore mieux formater les titres des pages

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -6,7 +6,7 @@
 
     <script src="https://cdn.tailwindcss.com"></script>
 
-    <title>{{ isset($title) ? $title . ' | '. config('app.name') : config('app.name')}}</title>
+    <title>{{ page_title($title ?? null) }}</title>
 </head>
 <body class="py-6 flex flex-col justify-between items-center min-h-screen">
     <main role="main" class="flex flex-col justify-center items-center">
