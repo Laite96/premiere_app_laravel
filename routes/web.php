b0ad573d7ccb14502b9e1273da4c7742b32f3a16
Mohamed Camara <mohamed669970215@gmail.com>
2022-08-02 01:28:54 +0000

    Ecrire d'une autre mainiere les routes

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
-
+/*
 Route::name('home')->get('', function () {
     return view('presentations.home');
 });
+*/
 
+Route::name('home')->get('', fn() => view('presentations.home'));
+
+Route::view('apropos', 'presentations.apropos')->name('apropos');
+
+/*
 Route::get('apropos', function ()
 {
    return view('presentations.apropos');
-})->name('apropos');
+})->name('apropos');*/
 
